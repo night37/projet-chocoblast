8d121a8374b3379e7fdde57ff14fef78d58466c9
mithridatem <mathieumithridate@adrar-formation.com>
2025-10-21 11:52:47 +0200

    feat : exemple route -> router

--- public/index.php
+++ public/index.php
@@ -8,4 +8,33 @@ $url = parse_url($_SERVER['REQUEST_URI']);
 //test soit l'url a une route sinon on renvoi à la racine
 $path = isset($url['path']) ? $url['path'] : '/';
 
-dd($url, $path);
+//Router
+switch ($path) {
+    case '/':
+        echo "Home";
+        break;
+    case '/login':
+        echo "login";
+        break;
+    case '/logout':
+        echo "Déconnexion";
+        break;
+    case '/chocoblast/add':
+        echo "Ajout d'un chocoblast";
+        break;
+    case '/chocoblast/all':
+        echo "Afficher tous les chocoblasts";
+        break;
+    case '/chocoblast/id':
+        echo "Affichage d'un chocoblast par son ID";
+        break;
+    case '/chocoblast/update/id':
+        echo "Modifier le chocoblast par son ID";
+        break;
+    case '/chocoblast/delete/id':
+        echo "Supprimer un chocoblast par son ID";
+        break;
+    default:
+        echo "Erreur 404";
+        break;
+}
